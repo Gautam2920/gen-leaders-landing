f34fa8defbc1302a91098b0a161bb11078b7ff5b
Gautam2920 <gautambhargava2004@gmail.com>
2026-03-23 11:26:42 +0530

    Moved the project to React

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import logo from '../assets/Logo.png'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-transparent py-3">
+            <div className="container">
+                <a className="navbar-brand fw-bolder fs-4 d-flex align-items-center" href="#">
+                    <img
+                        src={logo}
+                        alt="Gen Leaders Logo"
+                        width="40"
+                        height="40"
+                        className="me-2 rounded"
+                    />
+
+                    <span className="text-white">GEN</span>
+                    <span style={{ color: '#8aa2d3' }} className="ms-2">LEADERS</span>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
